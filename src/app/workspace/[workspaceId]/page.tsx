7914a3f98bf4b7c967ce3c17cc02f482aa812f16
saifmalik2004 <saifmalik7827@gmail.com>
2024-09-11 14:38:37 +0530

    add toolbar

--- src/app/workspace/[workspaceId]/page.tsx
+++ src/app/workspace/[workspaceId]/page.tsx
@@ -1,13 +1,16 @@
-import React from 'react'
-interface WorkspaceIdPageProps{
-    params:{
-        workspaceId:string
-    }
-}
+'use client'
 
-function WorkspaceIdPage({params}:WorkspaceIdPageProps) {
+import { useGetWorkspace } from "@/features/workspace/api/use-get-workspace"
+import { useWorkspaceId } from "@/hooks/use-workspace-id"
+import { useParams } from "next/navigation"
+
+
+
+function WorkspaceIdPage() {
+  const workspaceId=useWorkspaceId()
+  const {data}=useGetWorkspace({id:workspaceId})
   return (
-    <div>ID:{params.workspaceId}</div>
+    <div>ID:{JSON.stringify(data)}</div>
   )
 }
 
