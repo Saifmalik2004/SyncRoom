09d931178705d7377535ca4a1e251b51977439fb
saifmalik2004 <saifmalik7827@gmail.com>
2024-09-12 11:57:02 +0530

    meber functionality added

--- src/app/workspace/[workspaceId]/component/Workspace-switcher.tsx
+++ src/app/workspace/[workspaceId]/component/Workspace-switcher.tsx
@@ -35,7 +35,7 @@ const WorkspaceSwitcher = () => {
   
     return (
       <DropdownMenu>
-        <DropdownMenuTrigger>
+        <DropdownMenuTrigger  asChild>
           <Button className="size-9 relative overflow-hidden bg-[#ABABAD] hover:bg-[#ABABAD]/80 text-slate-800 font-semibold text-xl">
             {workspace?.name.charAt(0).toUpperCase()}
           </Button>
