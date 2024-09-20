33cee7e55da3a069de441949a1f9241b4eb92c79
saifmalik2004 <saifmalik7827@gmail.com>
2024-09-20 12:52:32 +0530

    infinite load

--- src/components/message-list.tsx
+++ src/components/message-list.tsx
@@ -6,6 +6,7 @@ import { useState } from "react";
 import { Id } from "../../convex/_generated/dataModel";
 import { useWorkspaceId } from "@/hooks/use-workspace-id";
 import { useCurrentMember } from "@/features/members/api/use-cuurent-member";
+import { Loader } from "lucide-react";
 
 const TIME_THRESHOLD=5;
 interface MessageListProps{
@@ -111,6 +112,32 @@ export const MessageList=({
 
       </div>
      ))}
+     <div className="h-1"
+     ref={(el)=>{
+      if(el){
+        const observer=new IntersectionObserver(
+          ([entry])=>{
+            if(entry.isIntersecting && canLoadMore){
+              loadMore()
+            }
+          },{
+            threshold:1.0
+          }
+        );
+        observer.observe(el)
+        return ()=> observer.disconnect()
+      }
+     }}
+     />
+     
+     {isLoadingMore &&(
+      <div className="text-center my-2 relative">
+      <hr className="absolute top-1/2 left-0 border-t border-gray-300"/>
+      <span className="relative inline-block bg-white px-4 rounded-full py-1 text-xs border border-gray-300 shadow-sm">
+        <Loader className="size-4 animate-spin"/>
+      </span>
+    </div>
+     ) }
      {variant ==="channel" && channelName && channelCreationTime&&(
       <ChannelHero
       name={channelName}
