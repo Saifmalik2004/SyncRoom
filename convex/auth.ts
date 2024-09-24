56f76da66564697b4b5379f054f11ed490f5a0c3
saifmalik2004 <saifmalik7827@gmail.com>
2024-09-24 21:56:50 +0530

    auuth session

--- convex/auth.ts
+++ convex/auth.ts
@@ -7,4 +7,9 @@ import Google from "@auth/core/providers/google";
 
 export const { auth, signIn, signOut, store } = convexAuth({
   providers: [GitHub,Google],
+  session: {
+    totalDurationMs: 30 * 24 * 60 * 60 * 1000, // 30 days
+    inactiveDurationMs: 24 * 60 * 60 * 1000,   // 24 hours
+  },
+  
 });
