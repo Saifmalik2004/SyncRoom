8bac321139b97c62c44a1eb7d871299acaa4976a
saifmalik2004 <saifmalik7827@gmail.com>
2024-09-25 18:40:14 +0530

    add hovereffect on toolbar

--- src/components/toolbar.tsx
+++ src/components/toolbar.tsx
@@ -25,7 +25,7 @@ const Toolbar=({
 }:ToolbarProps)=> {
   return (
     <div className="absolute top-0 right-5">
-        <div className="border bg-white">
+        <div className="group-hover:opacity-100 opacity-0 transition-opacity border bg-white">
            <EmojiPopover
            hint='Add reaction'
            onEmojiSelect={(emoji)=>handleReaction(emoji.native)}
